20f690a7937400056eb86cb24074cbcbf1e18ca7
jackdaw5 <s.sanazaro91@yahoo.com>
2022-10-07 15:34:20 -0500

    Added classes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,23 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBehavior : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public Player player;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void DetectPlayer()
-    {
-
     }
 }
