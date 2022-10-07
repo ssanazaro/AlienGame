420d0dda8078a5f4b8ef27c7df4058d84f61b54b
jackdaw5 <s.sanazaro91@yahoo.com>
2022-10-07 14:57:18 -0500

    Inital Push

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.tag.Equals("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
